2ecff080f1e415948d20ce6145ed375628f70208
Ji Ziqi <62317172+jh36714753@users.noreply.github.com>
2023-06-28 09:17:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_count():
   return (delta.days + 1)
 
 def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  next = datetime.strptime(str(date) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return ((next - today).days - 1)
